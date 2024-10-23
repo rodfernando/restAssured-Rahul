1c11c3927f042bf38477b192e5ca97bebab09f1c
Rodrigo <hazardrodrigo@hotmail.com>
2024-10-23 08:55:17 -0300

    first commit

---
+++ src/main/java/org/demoRestAssured/Main.java
@@ -0,0 +1,7 @@
+package org.demoRestAssured;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/Basics.java
@@ -0,0 +1,70 @@
+import files.Payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+public class Basics {
+
+    public static void main(String[] args) {
+
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        String response = given().
+                log().all().
+                queryParam("key", "qaclick123").
+                header("Content-Type", "application/json").
+                body(Payload.addPlace()).
+                when().
+                post("/maps/api/place/add/json").
+                then().
+//                log().all().
+                assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)").
+                extract().response().asString();// extrair todo o corpo da resposta. Tem que colocar numa variável
+
+        System.out.println(response);
+
+        JsonPath js = new JsonPath(response); // for parsing Json
+        String placeId = js.getString("place_id");
+        System.out.println("Este é o place_id = " + placeId);
+
+    // Update Place
+        String newAddress = "70 Summer walk, USA";
+
+        String testeJson = given().
+                log().all().
+                queryParam("key", "qaclick123").
+                header("Content-Type", "application/json").
+                body("{\n" +
+                        "\"place_id\":\""+placeId+"\",\n" +
+                        "\"address\":\"70 Summer walk, USA\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}\n").
+                when().
+                put("/maps/api/place/update/json").
+                then().
+                log().all().
+                assertThat().
+                statusCode(200).
+                body("msg", equalTo("Address successfully updated")).extract().response().asString();
+
+        JsonPath js2 = new JsonPath(testeJson);
+        String endereco = js2.getString("address");
+        System.out.println(endereco);
+
+//         Get Place
+        String getPlaceResponse = given().log().all().queryParam("key", "qaclick123")
+                .queryParam("placeid", placeId)
+                .when()
+                .get("/maps/api/place/get/json")
+                .then().assertThat().log().all().statusCode(200)
+                .extract().response().asString();
+
+        System.out.println(getPlaceResponse);
+
+//        JsonPath js1 = new JsonPath(getPlaceResponse);
+//        String actualAddress = js1.getString("address");
+//        System.out.println(actualAddress);
+    }
+}
