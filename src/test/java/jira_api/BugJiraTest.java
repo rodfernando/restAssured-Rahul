02c53e40e5f86e4237ece877aeb1ac5d7ec0adbd
Rodrigo <hazardrodrigo@hotmail.com>
2024-10-28 13:39:33 -0300

    git ignore working

--- src/test/java/jira_api/BugJiraTest.java
+++ src/test/java/jira_api/BugJiraTest.java
@@ -1,50 +1,106 @@
 package jira_api;
 
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
-import java.util.Properties;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import utils.ReusableMethods;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+
 public class BugJiraTest {
+
     public static void main(String[] args) {
-        
-        //Load config.properties here:
-        Properties prop = new Properties();
+        // Carrega as propriedades do arquivo config.properties
+        Properties prop = loadProperties("src\\test\\resources\\config.properties");
+
+        // Recupera os valores das propriedades e configura a URI base do RestAssured
         String authToken = prop.getProperty("AUTH_TOKEN");
         String baseUrl = prop.getProperty("BASE_URL");
+        RestAssured.baseURI = baseUrl;
 
+        // Cria uma nova issue no Jira e obtém o ID do bug criado
+        String issueId = createIssue(authToken);
 
-        RestAssured.baseURI= baseUrl;
-        String bugJiraResponse = given()
-        .header("Content-Type","application/json")
-        .header("Authorization",authToken)
-        .body("{\r\n" + //
-                        "    \"fields\": {\r\n" + //
-                        "       \"project\":\r\n" + //
-                        "       {\r\n" + //
-                        "          \"key\": \"SCRUM\"\r\n" + //
-                        "       },\r\n" + //
-                        "       \"summary\": \"Segundou!\",\r\n" + //
-                        "       \"issuetype\": {\r\n" + //
-                        "          \"name\": \"Bug\"\r\n" + //
-                        "       }\r\n" + //
-                        "   }\r\n" + //
-                        "}")
-        .log().all()
-        .when()
-        .post("/rest/api/3/issue")
-        .then()
-        .log().all().assertThat().statusCode(201)
-        .extract().response().asString();
-
-        JsonPath jsonData = ReusableMethods.rawToJson(bugJiraResponse);
-        String issueId = jsonData.get("id");
-        System.out.println("aqui está o id do bug criado:" + issueId);    
-
+        // Adiciona um anexo à issue criada
+        addAttachment(issueId, authToken);
     }
 
-}
+    /**
+     * Carrega as propriedades do arquivo de configuração.
+     *
+     * @param configPath Caminho para o arquivo config.properties.
+     * @return Propriedades carregadas.
+     */
+    private static Properties loadProperties(String configPath) {
+        Properties prop = new Properties();
+        try (FileInputStream fis = new FileInputStream(configPath)) {
+            prop.load(fis);
+            System.out.println("AUTH_TOKEN: " + prop.getProperty("AUTH_TOKEN"));
+            System.out.println("BASE_URL: " + prop.getProperty("BASE_URL"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao carregar o arquivo config.properties. Verifique o caminho e permissões.");
+        }
+        return prop;
+    }
 
-//Accept:application/json
+    /**
+     * Cria uma nova issue do tipo "Bug" no Jira.
+     *
+     * @param authToken Token de autorização.
+     * @return ID da issue criada.
+     */
+    private static String createIssue(String authToken) {
+        String response = given()
+                .header("Content-Type", "application/json")
+                .header("Authorization", authToken)
+                .body("{\r\n" +
+                        "    \"fields\": {\r\n" +
+                        "       \"project\": {\r\n" +
+                        "          \"key\": \"SCRUM\"\r\n" +
+                        "       },\r\n" +
+                        "       \"summary\": \"Teste Attachment\",\r\n" +
+                        "       \"issuetype\": {\r\n" +
+                        "          \"name\": \"Bug\"\r\n" +
+                        "       }\r\n" +
+                        "   }\r\n" +
+                        "}")
+                .log().all()
+                .when()
+                .post("/rest/api/3/issue")
+                .then()
+                .log().all()
+                .assertThat().statusCode(201)
+                .extract().response().asString();
+
+        JsonPath jsonData = ReusableMethods.rawToJson(response);
+        String issueId = jsonData.getString("id");
+        System.out.println("Aqui está o ID do bug criado: " + issueId);
+        return issueId;
+    }
+
+    /**
+     * Adiciona um anexo a uma issue no Jira.
+     *
+     * @param issueId   ID da issue onde o anexo será adicionado.
+     * @param authToken Token de autorização.
+     */
+    private static void addAttachment(String issueId, String authToken) {
+        given()
+                .pathParam("issueIdOrKey", issueId)
+                .header("Content-Type", "multipart/form-data")
+                .header("Authorization", authToken)
+                .header("X-Atlassian-Token", "no-check")
+                .multiPart("file", new File("src\\test\\java\\jsondatas\\jiracloud.3.postman.json"))
+                .log().all()
+                .when()
+                .post("/rest/api/3/issue/{issueIdOrKey}/attachments")
+                .then()
+                .log().all()
+                .assertThat().statusCode(200);
+    }
+}
